e80bb5f76f34dbc5e51cfd5bf5db6a6e45634ec1
ki-boop <karpakovkirill@gmail.com>
2023-05-23 14:49:07 +0400

    .

--- front/src/components/common/form/ButtonForm.vue
+++ front/src/components/common/form/ButtonForm.vue
@@ -1,12 +1,33 @@
 <template>
   <Button
-    label="Create"
+    :label="label"
     iconPos="right"
     icon="pi pi-check"
     size="small"
+    :disabled="disabled"
+    :loading="loading"
+    @click="click()"
   ></Button>
 </template>
 <script setup lang="ts">
+/* eslint-disable */
+export interface ButtonProps {
+  disabled?: boolean;
+  label: string;
+  loading?: boolean;
+}
 import Button from "primevue/button";
+const props = withDefaults(defineProps<ButtonProps>(), {
+  disabled: false,
+  label: "button",
+  loading: false,
+});
+
+function click() {
+  if (props.disabled) return;
+  emit("clicked");
+}
+
+const emit = defineEmits(["clicked"]);
 </script>
 <style></style>
