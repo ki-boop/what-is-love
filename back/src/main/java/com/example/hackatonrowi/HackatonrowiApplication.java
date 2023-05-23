fe14402ef4ef33817c78e4249de9bbccc546245a
NXTHlNG <nxthing.az@gmail.com>
2023-05-23 10:49:19 +0400

    init backend; add gitignore

---
+++ back/src/main/java/com/example/hackatonrowi/HackatonrowiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackatonrowi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonrowiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonrowiApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/example/hackatonrowi/HackatonrowiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackatonrowi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonrowiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
