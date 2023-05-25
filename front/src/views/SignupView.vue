99ab749c55c3487aba2154f91edae0f628f4dc2c
Maybeqeafie <algeev2017@mail.ru>
2023-05-25 06:37:44 +0300

    return css changed last

--- front/src/views/SignupView.vue
+++ front/src/views/SignupView.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="flex-wrapper">
+    <div class="circle1"></div>
+    <div class="circle2"></div>
+    <div class="circle3"></div>
+    <div class="circle4"></div>
+    <div class="circle5"></div>
+    <div class="circle6"></div>
+    <div class="circle7"></div>
     <div class="form-wrapper">
-      <div class="form-header">
-        <span><img src="\img\logo.svg" /></span>
-      </div>
       <div class="fields">
         <InputForm
           v-model="form.login"
@@ -11,13 +15,40 @@
           :placeholder="'Логин'"
           type="text"
         ></InputForm>
+        <div class="otstup"></div>
         <InputForm
           v-model="form.password"
           @change="changePassword($event)"
           :placeholder="'Пароль'"
           :type="'password'"
         ></InputForm>
+        <div class="remember-container">
+          <label for="remember-checkbox"
+                 style="color: rgba(255,255,255,0.5); display: flex; align-items: center; font-size: 15px">
+            <input type="checkbox" id="remember-checkbox"/>
+            <span class="checkmark"></span>
+            Запомнить
+          </label>
+          <button class="forgot-password"
+                  style="color: rgba(255,255,255,0.5); text-decoration:none; border: none; display: flex; align-items: center; font-size: 15px">
+            Не помню пароль
+          </button>
+        </div>
         <ButtonForm :label="'Войти'" @clicked="logIn()"></ButtonForm>
+        <button class="forgot-password" id="register"
+                style="color: rgba(255,255,255,0.5); text-decoration:none; border: none; display: flex; align-items: center; font-size: 15px">
+          Зарегистрироваться
+        </button>
+
+      </div>
+    </div>
+    <div class="text-right-phone">
+      <div class="text-right">
+        Здесь<br>
+        Вы точно<br>
+        найдете то,<br>
+        что Вам<br>
+        нужно.
       </div>
     </div>
   </div>
@@ -55,14 +86,16 @@ function auth() {
 </script>
 <style scoped lang="scss">
 .form-wrapper {
-  margin: 30vh;
   width: 400px;
   display: flex;
   flex-direction: column;
   align-items: center;
   border-radius: 10px;
   border: 1px solid rgba(34, 60, 80, 0.2);
-  padding: 20px;
+  padding: 70px;
+  background: linear-gradient(50deg, rgba(117, 26, 138, 0.5), rgb(133, 98, 194));
+  backdrop-filter: blur(10px);
+  margin: 20vh 1000px 30vh 30vh;
 
   .form-header {
     display: flex;
@@ -74,7 +107,161 @@ function auth() {
     margin-top: 20px;
     display: flex;
     flex-direction: column;
-    gap: 20px;
   }
+
+  body{
+    background-color: rgba(163, 238, 223, 0.7);
+  }
+  .p-button:hover {
+    background: #ff55b8;
+  }
+}
+
+.circle1 {
+  position: absolute;
+  background: radial-gradient(circle, rgba(237, 136, 228, 0.8), rgba(252, 124, 169, 0.8));
+  border-radius: 50%;
+  width: 300px;
+  height: 300px;
+  z-index: 0;
+  top: 0;
+  left: 0;
+  transform: translate(-20%, -10%);
+  filter: blur(2px);
+}
+.circle2 {
+  position: absolute;
+  background: radial-gradient(circle, rgb(198, 193, 247), rgb(145, 141, 209));
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  z-index: -1;
+  top: 150px;
+  left: 170px;
+  filter: blur(2px);
+}
+.circle3 {
+  position: absolute;
+  background: radial-gradient(circle, rgb(255, 199, 252), rgb(239, 131, 249));
+  border-radius: 50%;
+  width: 200px;
+  height: 200px;
+  z-index: -1;
+  top: 150px;
+  left: 1100px;
+  transform: translate(-20%, -10%);
+  filter: blur(1px);
+}
+.circle4 {
+  position: absolute;
+  background: radial-gradient(circle, rgb(198, 193, 247), rgb(145, 141, 209));
+  border-radius: 50%;
+  width: 70px;
+  height: 70px;
+  z-index: -1;
+  top: 580px;
+  left: 1150px;
+  transform: translate(-20%, -10%);
+  filter: blur(1px);
+}
+.circle5 {
+  position: absolute;
+  background: radial-gradient(circle, rgb(254, 189, 250), rgb(237, 133, 251));
+  border-radius: 50%;
+  width: 250px;
+  height: 250px;
+  z-index: -1;
+  top: 650px;
+  left: 650px;
+  transform: translate(-20%, -10%);
+  filter: blur(5px);
+}
+.circle6 {
+  position: absolute;
+  background: radial-gradient(circle, rgb(255, 188, 236), rgb(255, 108, 152));
+  border-radius: 50%;
+  width: 90px;
+  height: 90px;
+  z-index: 0;
+  top: 580px;
+  left: 1700px;
+  transform: translate(-20%, -10%);
+  filter: blur(2px);
+}
+.circle7 {
+  position: absolute;
+  background: linear-gradient(rgb(220, 149, 255), rgb(165, 168, 255));
+  border-radius: 50%;
+  width: 500px;
+  height: 500px;
+  z-index: -1;
+  top: 600px;
+  left: 1750px;
+  transform: translate(-20%, -10%);
+  filter: blur(10px);
+}
+.remember-container {
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+  margin-bottom: 30px;
+}
+.checkmark {
+  position: relative;
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border: 1px solid #ccc;
+  border-radius: 20%;
+  margin-right: 10px;
+}
+.checkmark::after {
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  background-color: #00b894;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+}
+input[type="checkbox"] {
+  position: absolute;
+  opacity: 0;
+}
+input[type="checkbox"]:checked ~ .checkmark::after {
+  opacity: 1;
+}
+.forgot-password {
+  background-color: transparent;
+  border: none;
+  color: #000;
+  text-decoration: underline;
+  cursor: pointer;
+  margin-left: 70px;
+}
+.otstup {
+  margin-bottom: 50px;
+}
+#register {
+  margin-left: 80px;
+}
+.text-right-phone{
+  position: absolute;
+  right: 0;
+  background: linear-gradient(150deg, rgb(85, 68, 159), rgba(250, 248, 248, 0.2));
+  backdrop-filter: blur(10px);
+  padding: 50% 20% 50% 50px;
+}
+.text-right{
+  color: white;
+  font-size: 70px;
+  margin-bottom: 130px;
+  font-family: "Gill Sans", sans-serif;
+  font-weight: 1000;
+  line-height: 88px;
 }
 </style>
--- front/src/views/SupportView.vue
+++ front/src/views/SupportView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="support">
+    <div class="circle1"></div>
+    <div class="circle2"></div>
+    <div class="circle3"></div>
+    <div class="circle4"></div>
+    <div class="circle5"></div>
+    <div class="circle6"></div>
+    <div class="circle7"></div>
     <h1> This is support page</h1>
   </div>
 </template>
@@ -9,5 +16,87 @@
 </script>
 
 <style scoped>
-
+.circle1 {
+  position: absolute;
+  background: radial-gradient(circle, rgba(237, 136, 228, 0.8), rgba(252, 124, 169, 0.8));
+  border-radius: 50%;
+  width: 300px;
+  height: 300px;
+  z-index: 0;
+  top: 0;
+  left: 0;
+  transform: translate(-20%, -10%);
+  filter: blur(2px);
+}
+.circle2 {
+  position: absolute;
+  background: radial-gradient(circle, rgb(198, 193, 247), rgb(145, 141, 209));
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  z-index: -1;
+  top: 150px;
+  left: 170px;
+  filter: blur(2px);
+}
+.circle3 {
+  position: absolute;
+  background: radial-gradient(circle, rgb(255, 199, 252), rgb(239, 131, 249));
+  border-radius: 50%;
+  width: 200px;
+  height: 200px;
+  z-index: -1;
+  top: 150px;
+  left: 1100px;
+  transform: translate(-20%, -10%);
+  filter: blur(1px);
+}
+.circle4 {
+  position: absolute;
+  background: radial-gradient(circle, rgb(198, 193, 247), rgb(145, 141, 209));
+  border-radius: 50%;
+  width: 70px;
+  height: 70px;
+  z-index: -1;
+  top: 580px;
+  left: 1150px;
+  transform: translate(-20%, -10%);
+  filter: blur(1px);
+}
+.circle5 {
+  position: absolute;
+  background: radial-gradient(circle, rgb(254, 189, 250), rgb(237, 133, 251));
+  border-radius: 50%;
+  width: 250px;
+  height: 250px;
+  z-index: -1;
+  top: 650px;
+  left: 650px;
+  transform: translate(-20%, -10%);
+  filter: blur(5px);
+}
+.circle6 {
+  position: absolute;
+  background: radial-gradient(circle, rgb(255, 188, 236), rgb(255, 108, 152));
+  border-radius: 50%;
+  width: 90px;
+  height: 90px;
+  z-index: 0;
+  top: 580px;
+  left: 1700px;
+  transform: translate(-20%, -10%);
+  filter: blur(2px);
+}
+.circle7 {
+  position: absolute;
+  background: linear-gradient(rgb(220, 149, 255), rgb(165, 168, 255));
+  border-radius: 50%;
+  width: 500px;
+  height: 500px;
+  z-index: -1;
+  top: 600px;
+  left: 1750px;
+  transform: translate(-20%, -10%);
+  filter: blur(10px);
+}
 </style>
