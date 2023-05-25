3eb47d8d4f5001375eea1ff9c0a352b26a4b258a
NXTHlNG <nxthing.az@gmail.com>
2023-05-25 04:41:52 +0400

    fixed

--- back/src/main/java/com/example/hackatonrowi/repository/ChatRepository.java
+++ back/src/main/java/com/example/hackatonrowi/repository/ChatRepository.java
@@ -10,5 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, UUID> {
-    List<Chat> findAllByProductAndManagerOnIsNull(Product product);
+    List<Chat> findAllByProductAndManagerIdOnIsNull(Product product);
 }
