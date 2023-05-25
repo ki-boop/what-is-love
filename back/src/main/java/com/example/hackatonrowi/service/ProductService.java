31efd75e747a01bed8c622cc9aafc945dee2aef4
NXTHlNG <nxthing.az@gmail.com>
2023-05-25 07:49:01 +0400

    product endpoint

--- back/src/main/java/com/example/hackatonrowi/service/ProductService.java
+++ back/src/main/java/com/example/hackatonrowi/service/ProductService.java
@@ -5,6 +5,7 @@ import com.example.hackatonrowi.entity.Product;
 import com.example.hackatonrowi.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class ProductService {
 
     public Optional<Product> getProductById(Long id) {
         return productRepository.findById(id);
+    }
+
+    public List<Product> getAll() {
+        return productRepository.findAll();
     }
 }
